0da7c33a837ba4f42c949f6397b2bca6c652119f
Alê-chan u.u <Al-chan u.u>
2016-07-01 11:02:20 -0300

    Queue 01 - Primeiro Passo

---
+++ main/queue-01/src/main/java/br/ufal/ic/queue-01/main.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int escolha, tam, run=0, preenchido=0, elemento;
+		
+		System.out.println("Diga o tamanho da fila");
+		tam = scan.nextInt();
+		
+		int[] fila = new int[tam];
+		
+		while(run == 0){
+			System.out.println("O que deseja fazer?");
+			System.out.println("1 - Add"); ///Adiciona um elemento
+			System.out.println("2 - Remove"); ///Remove e imprime o primeiro elemento
+			System.out.println("3 - Element");  ///Imprime o primeiro elemento
+			System.out.println("4 - Size"); ///Retorna o tamanho da lista
+			System.out.println("5 - Print"); ///Remove todos os elementos e imprime
+			System.out.println("6 - Quit"); ///Encerra o programa
+			
+			//escolha = ValidarValor.validarInt(1, 6);
+			escolha = scan.nextInt();
+			
+			switch(escolha){
+			case 1:
+				System.out.println("Add selecionado");
+				System.out.println("Digite o elemento que voce quer adicionar");
+				elemento = scan.nextInt();
+				if (preenchido == 0)
+				{
+					fila[preenchido] = elemento;
+					preenchido++;
+				}
+				else{
+					if(preenchido < tam)
+					{
+						fila[preenchido] = elemento;
+						preenchido++;
+					}
+					else
+					{
+						System.out.println("A fila esta cheia!");
+					}
+				}
+				break;
+				
+			case 2:
+				System.out.println("Remove selecionado");
+				break;
+				
+			case 3:
+				System.out.println("Element selecionado");
+				break;
+				
+			case 4:
+				System.out.println("Size selecionado");
+				break;
+				
+			case 5:
+				System.out.println("Print selecionado");
+				break;
+				
+			case 6:
+				run = 1;
+				break;
+			}
+		}
+	}
+}
